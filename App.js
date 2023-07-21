91855535039fc151a95ff0b3b1e5045274b72669
Himanshu Kushwaha <124574943+Web-Dev-Kush@users.noreply.github.com>
2023-07-21 16:36:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h3>Why Choose Us?</h3>
+        <h2>Why the industry chooses Clickdee?</h2>
+      </div>
+      <div>
+        <p>
+          We understand performance marketing from every angle and every stage
+          of the funnel. Our clients trust that we know what metrics move their
+          business towards growth. Our publisher and affiliate partners know
+          that we make maximum revenue and ROAS a main focus when growing our
+          partnerships.
+        </p>
+      </div>
+      <div className="container">
+        <div className="zoom">
+          <h2>Choose your local targeting</h2>
+          <p>Our targetted and tracked calls</p>
+        </div>
+        <div className="zoom">
+          <h2>Track your conversion</h2>
+          <p>We use the most progressive</p>
+        </div>
+        <div className="zoom">
+          <h2>Customized Campaigns</h2>
+          <p>
+            Audiences, budgets, and goals: you decide the criteria for your
+            digital ad campaign, and well do the rest.
+          </p>
+        </div>
+        <div className="zoom">
+          <h2>Get Dedicated Support Team</h2>
+          <p>
+            We constantly monitor quality control! Our affiliate network of
+            publishers are digital ad experts that have been vetted.
+          </p>
+        </div>
+        <div className="zoom">
+          <h2>Quality Assurance</h2>
+          <div id="#DivForHoverItem">
+            <div id="HiddenText">
+              <p>
+                We constantly monitor quality control! Our affiliate network of
+                publishers are digital ad experts that have been vetted.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
